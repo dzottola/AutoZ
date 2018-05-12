2db9284ea57d44739f994b8a5ac7ccd99fb48c2d
diego.zottola <diego.zottola@alphapaymentscloud.com>
2018-05-12 22:39:57 +0200

    Webdriver migrated to Chrome

--- src/main/java/com.zero.autoz/StartWebDriver.java
+++ src/main/java/com.zero.autoz/StartWebDriver.java
@@ -1,18 +1,44 @@
 package com.zero.autoz;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 
 public class StartWebDriver {
+   static WebDriver driver;
+
+    @FindBy(how = How.ID, using = "user")
+    static WebElement userTextBox;
+
+    @FindBy(how = How.ID, using = "password")
+    static WebElement passwordTextBox;
+
+    @FindBy (how = How.XPATH, using = "//button[@type='submit']")
+    static WebElement buttonSubmit;
 
     @Test
-    public void startWebDriver() {
-        WebDriver driver = new FirefoxDriver();
-        driver.navigate().to("http://google.com");
+    public void startWebDriver() throws InterruptedException {
+
+
+        WebDriver driver;
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8000/#!/login");
+        PageFactory.initElements(driver, this);
+
         System.out.println(driver.getTitle());
-        Assert.assertTrue(driver.getTitle().startsWith("Goo"), "deberia estar en google.");
+        Thread.sleep(10000);
+
+        //Test
+        userTextBox.sendKeys("User");
+        passwordTextBox.sendKeys("fakepass");
+        buttonSubmit.click();
     }
+
 }
